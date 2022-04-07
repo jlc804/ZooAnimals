d4436046a0c538d9b2b40236d30eaf0da875c5a8
Trey Cox (justcox) <justcox@cisco.com>
2022-04-07 17:34:25 -0400

    commit

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ class ZooAnimals:
         self.weight = weight
         self.height = height
         self.volume = weight * height
+        self.whatever = 0
 
+    def fullname(self):
+        return self.whatever
+
+    def __str__(self):
+        return '45'
 
 zebra = ZooAnimals(10,12)
 coyote = ZooAnimals(15,25)
 
 print(zebra.volume, coyote.volume)
+print(zebra.fullname())
 
+print(ZooAnimals.fullname(zebra))
+print(ZooAnimals(3,4))
