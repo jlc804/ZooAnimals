081be1bbca2cc0d7d74398256a596f4624fab3a0
Trey Cox (justcox) <justcox@cisco.com>
2022-04-09 21:55:31 -0400

    commit

--- main.py
+++ main.py
@@ -1,41 +1,39 @@
-class ZooAnimals:
-        """
-        Class for Zoo Animals
-        :params:
-            self.animalType - Animal type that starts with 'S'
-            self.idTag:int - Animal ID tag
-            self.minTemperature:float - minimum temperatures for animal's enclosure (in Celsius)
-            self.maxTemperature:float - maximum temperatures for animal's enclosure (in Celsius)
-        """
-    def __init__(self):
-        self.animalType = coyote
-        self.idTag = None
-        self.minTemperature = 20
-        self.maxTemperature = 100
+class Coyote:
+    """Class for Coyote"""
 
-    def __str__(self):
-        return 'test'
+    def __init__(self):
+        self.animalType = 'coyote'
+        self.idTag = None
+        self.minTemperature = 0
+        # Should be in Celsius
+        self.maxTemperature = 40
+        #Should be in Celsius
 
     def getAnimalType(self):
+        """Return animal type"""
         return self.animalType
 
     def getIdTag(self):
+        """Return ID Tag"""
         return self.idTag
 
     def setIdTag(self, anIdTag):
+        """Sets ID Tag"""
         self.idTag = anIdTag
         return self.idTag
 
     def getMinTemperature(self):
+        """Returns Minimum Temperature in Celsius"""
         return self.minTemperature
 
     def getMaxTemperature(self):
+        """Returns Minimum Temperature in Celsius"""
         return self.maxTemperature
 
-coyote = ZooAnimals('Coyote',25,12,12)
-
+coyote = Coyote()
 print(coyote.getAnimalType())
 print(coyote.getIdTag())
 coyote.setIdTag(100)
 print(coyote.getIdTag())
-print(coyote)
+print(coyote.getMinTemperature())
+print(coyote.getMaxTemperature())
