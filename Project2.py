31cbae61fc4799f978a51ab3b693cccfd053518f
Trey Cox (justcox) <justcox@cisco.com>
2022-04-27 15:43:23 -0400

    commit

--- Project2.py
+++ Project2.py
@@ -1,25 +1,20 @@
 
 def main():
-    seats_row0 = ['Empty' for k in range(5)]
+    seats_row0 = ['Robbie', 'Brett', 'Empty', 'Empty', 'Ashlee']
     seats_row1 = ['Empty' for k in range(5)]
-    seats_row2 = ['Empty' for k in range(5)]
-    seats_row3 = ['Empty' for k in range(3)]
-    seats_row4 = ['Empty' for k in range(2)]
+    seats_row2 = ['Jason', 'Brent', 'Empty', 'Empty', 'Empty']
+    seats_row3 = ['Drew', 'Ryan', 'Mondaine']
+    seats_row4 = ['Paul', 'James']
     seats = [seats_row0, seats_row1, seats_row2, seats_row3, seats_row4]
-    #test = [[seat0],[seat2]]
     seating_chart = seating_chart_class(seats)
-    seating_chart.addStudent('Trey', 4,0)
+    seating_chart.addStudent('Trey', 2,3)
     seating_chart.displaySeatingChart()
-
-
-
-
+    teams = seating_chart.assignTeams()
+    seating_chart.displayTeams(teams)
 
 
 class seating_chart_class:
     def __init__(self, seats):
-        self.num = 20
-        self.name = 'Student'
         self.seats = seats
 
     def addStudent(self, student_name, target_seat_row, col):
@@ -27,29 +22,35 @@ class seating_chart_class:
 
     def displaySeatingChart(self):
         for row in range(len(self.seats)):
-            # print('Row: '+str(row), end='')
             for element in range(len(self.seats[row])):
-                print('\t' + str(self.seats[row][element]), end='')
+                print('\t' + str(self.seats[row][element]), end='\t')
             print('')
 
-    def assignTeams(selfs):
 
+    def assignTeams(self):
+        team0 = []
+        team1 = []
+        team2 = []
+        team3 = []
+        team4 = []
+        teams = [team0, team1, team2, team3, team4]
 
+        for row in self.seats:
+                i = 0
+                for element in row:
+                    if str(element) != 'Empty':
+                        teams[i].append(element)
+                    i+=1
+        return teams
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def displayTeams(self, teams):
+        i=0
+        for team in teams:
+            print('Team' + str(i), end='\n')
+            for members in team:
+                print(members,end=' ')
+            print('')
+            i+=1
 
 
 
