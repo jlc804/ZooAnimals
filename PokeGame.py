dde52e41383fde9c6acd1540956b689db9ee2f31
Trey Cox (justcox) <justcox@cisco.com>
2022-06-10 14:57:13 -0400

    commit

---
+++ PokeGame.py
@@ -0,0 +1,10 @@
+class PokeGame:
+    def __int__(self):
+        self.game_master = []
+        self.setup()
+
+
+
+
+    def setup(self):
+
--- Project 3.py
+++ Project 3.py
@@ -11,8 +11,6 @@ class Pokemon:
         self.hp = 50
         self.status_condition = None
         self.prob = None
-        self.strong_against = []
-        self.weak_against = []
 
     def __str__(self):
         return self.name + ' ' + self.trainer
@@ -32,34 +30,30 @@ class Pokemon:
             print(self.name, ' fainted!')
 
 class Psychic(Pokemon):
-
+#Psychic Pokemon Class provided by Matthew Skokos
     def __init__(self, name, trainer, hp):
         super().__init__(name, trainer)
         self.hp = hp
         self.basic_attack = 'Psychic Shift'
         self.prob = 1.0
 
-
-    def __str__(self):
-        return f'I am {self.name} and I can do {self.basic_attack}'
-
     def attack(self, other):
         if  self.status_condition == None:
             self.speak()
-        if isinstance(other, (Poison, Fighting)):
-            self.speak()
-            print(self.name, ' used ', self.basic_attack, '!')
-            other.receive_damage(self.damage * 2)
-        elif isinstance(other, (Bug, Ghost, Dark)):
-            self.speak()
-            print(self.name, ' used ', self.basic_attack, '!')
-            other.receive_damage(self.damage / 2)
-        elif isinstance(other, Psychic):
-            other.receive_damage(self.damage)
+            print(self.name, 'used', self.basic_attack, '!')
+            if isinstance(other, (Poison, Fighting)):
+                other.receive_damage(self.damage * 2)
+            elif isinstance(other, (Bug, Ghost, Dark)):
+                other.receive_damage(self.damage / 2)
+            else:
+                other.receive_damage(self.damage)
 
-        if random() < self.prob and type(other) != Psychic:
-            other.paralyzed = True
-            print(other.name, 'is paralyzed')
+        if random() < self.prob and not isinstance(other, Psychic):
+            other.status_condition = 'paralyzed'
+            print(other.name + ' is ' + other.status_condition)
+
+    def __str__(self):
+        return f'I am {self.name} and I can do {self.basic_attack}'
 
 
 class Bug(Pokemon):
@@ -108,15 +102,11 @@ class Fairy(Pokemon):
 Butterfree = Bug('Butterfree', 'Trey',100)
 
 
-print(Butterfree)
-
-
-
 p1 = Psychic('Mewtwo', 'Jen', 100)
 p2 = Psychic('Gengar', 'James', 40)
 
 print(p1.hp)
-p2.attack(p1)
+p2.attack(Butterfree)
 print(p2.hp)
 print(p1.hp)
 
