590e0ea99794bd2eb3ff69f5b31da2e569951ecc
Trey Cox (justcox) <justcox@cisco.com>
2022-06-10 13:35:47 -0400

    commit

--- Project 3.py
+++ Project 3.py
@@ -9,14 +9,20 @@ class Pokemon:
         self.trainer = trainer
         self.level = 1
         self.hp = 50
-        self.status_condition = ''
+        self.status_condition = None
+        self.prob = None
         self.type = ''
+        self.strong_against = ['Grass', 'Psychic', 'Dark']
+        self.weak_against = ['Fighting', 'Flying', 'Poison', 'Ghost', 'Fire', 'Fairy']
+
+    def __str__(self):
+        return self.name + '' + self.trainer
 
     def speak(self):
         print(self.name + '!')
 
     def attack(self, other):
-        if not self.paralyzed:
+        if self.status_condition == None:
             self.speak()
             print(self.name, ' used ', self.basic_attack, '!')
             other.receive_damage(self.damage)
@@ -26,16 +32,25 @@ class Pokemon:
         if self.hp == 0:
             print(self.name, ' fainted!')
 
+class TestType(Pokemon):
+    def __init__(self, name, trainer, hp):
+        super().__init__(name, trainer)
+        self.level = 1
+        self.hp = hp
+        self.prob = .1
+        self.type = 'Fighting'
+        self.strong_against = ['Grass', 'Psychic', 'Dark']
+        self.weak_against = ['Fighting', 'Flying', 'Poison', 'Ghost', 'Fire', 'Fairy']
+
+
 
 class BugType(Pokemon):
     def __init__(self, name, trainer, hp):
-        self.name = name
-        self.trainer = trainer
-        self.level = 1
+        super().__init__(name, trainer)
         self.hp = hp
-        self.status_condition = ''
         self.basic_attack = 'Signal Beam'
         self.prob = .1
+        self.type = 'Bug'
         self.strong_against = ['Grass', 'Psychic', 'Dark']
         self.weak_against = ['Fighting', 'Flying', 'Poison', 'Ghost', 'Fire', 'Fairy']
 
@@ -43,19 +58,30 @@ class BugType(Pokemon):
         if  self.status_condition == None:
             self.speak()
             print(self.name, 'used', self.basic_attack, '!')
-            other.receive_damage(self.damage)
+            if other.type in self.strong_against:
+                print('yes')
+                other.receive_damage(self.damage*2)
+            elif other.type in self.weak_against:
+                other.receive_damage(self.damage/2)
+            else:
+                other.receive_damage(self.damage)
         if random() < self.prob:
             other.status_condition = 'confused'
             print(other.name + ' is ' + other.status_condition)
 
     def __str__(self):
-        pass
+        return super().__str__() + " hp" + str(self.name) + ")"
+
 
 
 
 Butterfree = BugType('Butterfree', 'Trey',100)
-Charizard =0
+Charizard =TestType('Char', 'Trey', 100)
 print(Butterfree.hp)
-Butterfree.attack(Butterfree)
+Butterfree.attack(Charizard)
+Charizard.attack(Butterfree)
+print(Butterfree.hp)
+print(Charizard.hp)
 
+print(Butterfree)
 
