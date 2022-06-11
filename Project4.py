2e2eee08173644e6983ff92cb70ba00d75c03cab
Trey Cox (justcox) <justcox@cisco.com>
2022-06-11 18:00:15 -0400

    commit

---
+++ Project4.py
@@ -0,0 +1,20 @@
+class Caesar:
+    def __init__(self):
+        pass
+
+    def caesar_encode(self):
+        pass
+
+    def caesar_decode(self):
+        print('Hi')
+        pass
+
+
+def process_file():
+    print('Processed')
+    pass
+
+
+test = Caesar()
+test.caesar_decode()
+process_file()
